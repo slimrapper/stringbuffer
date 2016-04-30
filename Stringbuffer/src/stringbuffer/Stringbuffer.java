b47fd4712531f702059217b2d8b220341683e75c
slimrapper <slimrapper112@gmail.com>
2016-04-29 23:04:21 -0500

    buffer

---
+++ Stringbuffer/src/stringbuffer/Stringbuffer.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stringbuffer;
+
+/**
+ *
+ * @author slim
+ */
+public class Stringbuffer {
+   
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+ StringBuffer buffer = new StringBuffer("hello world");
+System.out.println(buffer.length());
+System.out.println(buffer.capacity());       
+        
+    }
+    
+}
